039231e6ac3578adcfead17721b5936f2ba15079
sanvi-2014girl <goswamilipi82@gmail.com>
2024-11-16 11:32:54 +0530

    Bit wise

---
+++ main.py
@@ -0,0 +1,19 @@
+num1 = 10
+num2 = 4
+#Using AND operator
+print("num1 & num2 =",num1 & num2)
+#Using OR operator
+print("\nnum1 | num2 |=",num1 | num2)
+#Using NOT operator
+print("\n~num1 =", ~num1)
+num1 = 10
+num2 = 4
+#using right shift operator on num1
+print("\nnum >> 1 =", num1 >> 1)
+#using right shift operator on num 2
+print("\nnum2 >> 1 =", num2 >> 1)
+num1 = 10
+num2 = 4
+print("\nnum << 1 =", num1 << 1)
+#using right shift operator on num 2
+print("\nnum2 << 1 =", num2 << 1)
